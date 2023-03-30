d8d5aed17c8df38caeb8910315f484c098d92cf4
Gevorg <g.danelyan@worksolutions.ru>
2023-03-30 18:35:07 +0300

    Исправлен экспорт модулей в index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import "reflect-metadata";
 
-export * from "shared/config/theme";
-export * from "shared/appServices";
+export { theme } from "shared/config/theme";
+export { useAppServices, AppServices } from "shared/appServices";
