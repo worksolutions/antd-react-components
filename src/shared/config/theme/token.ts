118d18048538ec2ecf302e9a93e1d1bf7864d4db
Gevorg <g.danelyan@worksolutions.ru>
2023-04-10 15:53:11 +0300

    Заменены цвета типографики

--- src/shared/config/theme/token.ts
+++ src/shared/config/theme/token.ts
@@ -1,3 +1,8 @@
 import { ThemeConfig } from "antd";
 
-export const token: ThemeConfig["token"] = {};
+export const token: ThemeConfig["token"] = {
+  colorText: "#313D4C",
+  colorTextSecondary: "#617894",
+  colorTextTertiary: "#96A6BA",
+  colorTextQuaternary: "#D7DDE5",
+};
