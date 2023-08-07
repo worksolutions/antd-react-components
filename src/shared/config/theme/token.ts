b36bf21cc192f750ca8759f25b7f9a8392e3878d
Alexandr Klykov <a.klykov@worksolutions.ru>
2023-08-07 14:46:25 +0300

    Изменены токены цветов плейсхолдеров и бордеров, тени

--- src/shared/config/theme/token.ts
+++ src/shared/config/theme/token.ts
@@ -5,4 +5,7 @@ export const token: ThemeConfig["token"] = {
   colorTextSecondary: "#617894",
   colorTextTertiary: "#96A6BA",
   colorTextQuaternary: "#D7DDE5",
+  colorTextPlaceholder: "#617894",
+  colorBorder: "#96A6BA",
+  boxShadowTertiary: "0px -40px 48px -16px rgba(34, 60, 80, 0.18)"
 };
