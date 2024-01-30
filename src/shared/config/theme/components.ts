2c6cd2913e79a233ca24ef25652457ac0f7e9729
TusksAndTasks <86926386+TusksAndTasks@users.noreply.github.com>
2024-01-30 12:19:33 +0300

    Изменен фон футера лейаута (#5)
    * @types/antd заменен на antd зависимость, добавлена зависимость eslint-config-react-app
    
    * Убраны и заменены устаревшие свойства темы компонентов
    
    * Изменен фон футера лейаута
    
    ---------
    
    Co-authored-by: Alexandr Klykov <a.klykov@worksolutions.ru>

--- src/shared/config/theme/components.ts
+++ src/shared/config/theme/components.ts
@@ -2,13 +2,10 @@ import { ThemeConfig } from "antd";
 
 export const components: ThemeConfig["components"] = {
   Layout: {
-    colorBgBody: "transparent",
-    colorBgHeader: "transparent",
+    bodyBg: "transparent",
+    headerBg: "transparent",
     colorBgContainer: "transparent",
     colorBgElevated: "transparent",
-  },
-  Typography: {
-    sizeMarginHeadingVerticalEnd: 0,
-    sizeMarginHeadingVerticalStart: 0,
+    footerBg: "transparent"
   },
 };
