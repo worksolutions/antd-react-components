56a73cc36ac9566d8553be55d146f839e3b62825
Gevorg Danelyan <g.danelyan@worksolutions.ru>
2024-02-13 12:08:10 +0300

    feat(customize-theme)

--- src/shared/config/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import { ThemeConfig } from "antd";
-
-import { components } from "./components";
-import { token } from "./token";
-
-export const theme: ThemeConfig = {
-  components,
-  token,
-}
